d966577b909c949249171b3b6399dc4f0a784cec
Joe Langston <joelangston81@gmail.com>
2020-06-15 13:09:18 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const mainElement = document.querySelector('main')
+
+const kata1heading = document.createElement('h1')
+kata1heading.append('Kata 1')
+mainElement.append(kata1heading)
+let counter = 1
+  while (counter <= 20 ) {
+    console.log (counter)
+    counter += 1
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Mike Reames, Katrine Jackson, Samantha Taylor//
